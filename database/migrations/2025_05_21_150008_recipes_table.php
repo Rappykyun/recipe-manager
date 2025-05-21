9ea86b6de41c3995748a781d0441368ac8b3419b
Rappykyun <rakuichijo155@gmail.com>
2025-05-21 23:18:58 +0800

    Add initial project structure and made the recipe table

---
+++ database/migrations/2025_05_21_150008_recipes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+
+            
+            $table->id('recipe_id');
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->string('title');
+
+            $table->text('description');
+
+            $table->enum('category',['Breakfast', 'Launch' , 'Dinner', 'Dessert', 'Snack']);
+
+            $table->integer('prep_time')->nullable();
+
+            $table->boolean('is_public')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
